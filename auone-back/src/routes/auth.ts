a88df3ee8666127557df8ca33cb2b61705028918
Mari1916 <mariana.freitas1916@gmail.com>
2025-10-14 09:57:15 -0300

    movendorota de perfil para auth

--- auone-back/src/routes/auth.ts
+++ auone-back/src/routes/auth.ts
@@ -72,6 +72,42 @@ router.post("/login", async (req: Request, res: Response) => {
   }
 });
 
+//ROTA DE DISPOSITIVOS
+router.post('/', async (req, res) => {
+  try {
+    const { nome, deviceId, usuarioId } = req.body;
+
+    if (!nome || !deviceId || !usuarioId) {
+      return res.status(400).json({ erro: 'Campos obrigatórios ausentes' });
+    }
+
+    const dispositivo = await prisma.dispositivo.create({
+      data: {
+        nome,
+        deviceId,
+        usuario: {
+          connect: { id: usuarioId },
+        },
+      },
+    });
+
+    res.status(201).json(dispositivo);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json({
+        erro: 'Erro ao cadastrar dispositivo',
+        detalhe: error.message,
+      });
+    } else {
+      res.status(500).json({
+        erro: 'Erro desconhecido',
+        detalhe: String(error),
+      });
+    }
+  }
+});
+
+
 // TESTE ROTA DE PERFIL
 
 router.get('/perfil', async (req, res) => {
