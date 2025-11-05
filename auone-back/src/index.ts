de7c0f9d738b9b0294bc9a689637c80b2ce70af6
Mari1916 <mariana.freitas1916@gmail.com>
2025-11-05 05:41:11 -0300

    arrumando endpoin cadastro dnv

--- auone-back/src/index.ts
+++ auone-back/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
 
 import authRoutes from "./routes/auth";
 import dispositivosRoutes from "./routes/dispositivos";
