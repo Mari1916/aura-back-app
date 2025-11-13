54d084a6d21c5632776a3782f4c377fe1e151221
Mari1916 <mariana.freitas1916@gmail.com>
2025-11-13 14:15:00 -0300

    gerando nova tabela no banco referente ao chat

---
+++ auone-back/prisma/migrations/20251113171333_tabela_do_chat/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Conversa" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Conversa_pkey" PRIMARY KEY ("id")
+);
