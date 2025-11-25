7b0282824d321cfd1a31ec1272876ba0909810b4
Mari1916 <mariana.freitas1916@gmail.com>
2025-11-25 10:15:13 -0300

    REMOVENDO DIST NOVAMENTE

--- auone-back/dist/middlewares/notFound.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFound = void 0;
-const notFound = (req, res) => {
-    res.status(404).json({ erro: "Rota não encontrada" });
-};
-exports.notFound = notFound;
--- auone-back/dist/routes/dispositivos.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const dispositivosController_1 = require("../controllers/dispositivosController");
-const router = (0, express_1.Router)();
-// POST /api/dispositivo
-router.post("/", dispositivosController_1.cadastrarDispositivo);
-exports.default = router;
