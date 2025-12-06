3f48cb4823542ef4b3aa4e069eca2e117508dd8a
Mari1916 <mariana.freitas1916@gmail.com>
2025-12-06 09:16:38 -0300

    alterando id do teste

--- testChat.ts
+++ testChat.ts
@@ -4,7 +4,7 @@ async function test() {
   const res = await axios.post(
     "https://aura-back-app.onrender.com/api/chat/message", // URL do Render
     {
-      userId: "1", // agora como string
+      userId: "22f7cfab-56cd-4032-b43f-a3738075972b", // agora como string
       message: "Minha planta está com folhas amareladas, o que pode ser?"
     }
   );
